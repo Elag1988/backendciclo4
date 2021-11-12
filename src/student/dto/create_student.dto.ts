bc688c8120481410eab3a930ed3b48a644107b44
Elag1988 <90296257+Elag1988@users.noreply.github.com>
2021-11-12 11:48:26 -0500

    Testing New Branch

--- src/student/dto/create_student.dto.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { IStudent } from '../interfaces/student.interface'
-export class CreateStudentDTO implements IStudent{
-  readonly code: number;
-    readonly name: string;
-    readonly photoURL: string;
-    readonly createdAt: Date;
-
-}
