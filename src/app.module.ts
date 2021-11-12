f1c4d727a633c2a171287562b2340b808407de5c
Elag1988 <90296257+Elag1988@users.noreply.github.com>
2021-11-12 11:50:20 -0500

    BackEnd Project with some issues

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { StudentModule } from './student/student.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { InventarioModule } from './inventario/inventario.module';
@@ -10,7 +9,6 @@ import { DomicilioModule } from './domicilio/domicilio.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    StudentModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DATABASE_HOST,
